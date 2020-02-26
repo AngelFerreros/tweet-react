0603b5124632008c194ff8992cfad9eb4cb81e72
Angel Ferreros <AngelFerreros@TJs-MacBook-Pro.local>
2020-02-26 17:09:55 +0800

    Initital commit - able to render the tweet texts

--- src/client/index.jsx
+++ src/client/index.jsx
@@ -1,13 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
 import tweets from 'tweets'
+
+class Tweet extends React.Component{
+  render() {
+      let tweets = this.props.tweet.map (tweet => <li>{tweet.text}</li>)
+    return(
+      <div>
+        <ul>
+        {tweets}
+        </ul>
+      </div>
+      );
+  }
+}
+
+class User extends React.Component{
+  render (){
+    return (
+    <div className = "screenName">
+      <h2>{this.props.name}</h2>
+    </div>
+      );
+  }
+}
 
 class App extends React.Component {
   render() {
+
     return (
       <div>
-        tweets
+
+        <Tweet tweet= {tweets.tweets}/>
       </div>
     );
   }
